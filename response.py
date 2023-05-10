29e3585927cb132532f027634eeef9057a974e52
koleq(WORK-D) <o.kurz@atlas.cz>
2023-05-10 11:11:53 +0200

    le tried again after reading response on reddit

--- response.py
+++ response.py
@@ -1,8 +1,8 @@
 def serverip():
-    ip = "192.168.1.151"
+    ip = "192.168.2.60" #"192.168.1.151"
     return ip
 
 def give_data():
     contenttype = "text/json"
-    return_data = b'{messagetext: [cost: 1, "text","text", goof]}'
+    return_data = b'1' #b'{messagetext: [cost: 1, "text","text", goof]}'
     return (contenttype, return_data)
