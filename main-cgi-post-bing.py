b85e55442dee4ae7f61be41a9831b184b22b9a0f
koleq(MacBook) <o.kurz@atlas.cz>
2023-05-14 22:09:00 +0200

    Implementing new changes
    I know these commits kinda suck I would do better but nah
    I just commit when I'm done with the testing and implementing for the nigth

--- main-cgi-post-bing.py
+++ main-cgi-post-bing.py
@@ -5,6 +5,18 @@ from importlib import reload
 import response
 # import socketserver
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('192.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 # Define a custom handler class that inherits from BaseHTTPRequestHandler
 class MyHandler(BaseHTTPRequestHandler):
 
@@ -21,14 +33,7 @@ class MyHandler(BaseHTTPRequestHandler):
         self.wfile.write(b'<html><body><h1>Hello from python!</h1></body></html>')
 
     # Override the do_POST method to handle POST requests
-    def do_POST(self):
-
-        try:
-            reload(response)
-            contentype, return_data = response.give_data()
-        except Exception as e:
-            print(e)
-        
+    def do_POST(self):        
         # Check if the request is a cgi post request
         if self.path.startswith('/cgi-bin/'):
             # Read the request data
@@ -42,27 +47,64 @@ class MyHandler(BaseHTTPRequestHandler):
             # Send a 200 OK response
             self.send_response(200)
 
-            # Open a file and read it's data as bytes
-            # with open('test.gif', 'rb') as f:
-                # image_data = f.read()
-
+            ## Here is implemented everything that gamstat figured out on reddit
             if "checkmessages" in data:
-                self.send_header('Content-type', "text/json")
+                print("checkmessages")
+                self.send_header('Content-type', "plain/text")
                 self.end_headers()
-                self.wfile.write(b'["messagetext": ["cost": "this is cost", "more": "this is more", "license": "this is license"]]')
+                return_data = b'["messagetext": ["cost": "this is cost", "more": "this is more", "license": "this is license", "emailtext": "this is emailtext", "warnpages": "warnpages"]]'
+            elif "createuserid" in data:
+                print("createuserid")
+                self.send_header('Content-type', "plain/text")
+                self.end_headers()
+                return_data = b'["userID": "123"]'
+            elif "saveother" in data:
+                print("saveother")
+                self.send_header('Content-type', "plain/text")
+                self.end_headers()
+                return_data = b''  # yep just empty reply, but I still send headder
+            elif "firsttimeprojector" in data:
+                print("firsttimeprojector")
+                self.send_header('Content-type', "plain/text")
+                self.end_headers()
+                return_data = b'["messagetext": [""]]'
+            elif "getlasthistory" in data:
+                print("getlasthistory")
+                self.send_header('Content-type', "plain/text")
+                self.end_headers()
+                return_data = b'["lasthistory": ""]'
+            elif "getticketsleft" in data:
+                print("getticketsleft")
+                self.send_header('Content-type', "plain/text")
+                self.end_headers()
+                return_data = b'["ticketsleft": 66]'
+            elif "createusername" in data:
+                print("getticketsleft")
+                self.send_header('Content-type', "plain/text")
+                self.end_headers()
+                return_data = b'["userName": "namefromreply"]'
             else:
+                print("!!! unimplemented !!!")
+                try:
+                    reload(response)
+                    contentype, return_data = response.give_data()
+                except Exception as e:
+                    print(e)
+                ## Here is implemented my dynamic testing
                 # Send headers
                 self.send_header('Content-type', contentype)  # image/gif, text/plain
                 # self.send_header('Content-Length', len(image_data))
                 self.end_headers()
 
-                # Write some content to the response
-                # self.wfile.write(image_data)
-                self.wfile.write(return_data)  # {"stuff": {"cost": 1, "more": 1, "license": 1}}
-            print(return_data)
+            # Write some content to the response
+            # self.wfile.write(image_data)
+            self.wfile.write(return_data)
+            print(str(return_data)+"\n")
+
+local_ip = get_local_ip()
 
 # Define the port on which you want to run the server
-host = response.serverip()
+host = local_ip
 port = 80
 
 # Create a server object using the handler class, host ip and port number
--- response.py
+++ response.py
@@ -1,7 +1,3 @@
-def serverip():
-    ip = "192.168.2.60" #"192.168.1.151"
-    return ip
-
 def give_data():
     contenttype = "text/json"
     return_data = b'["messagetext": []]'
